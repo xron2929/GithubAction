460c5c8710fbf3a98028e01bdc0c943ef3f3cfde
consistent <xron2929@gmail.com>
2023-01-13 06:59:03 +0900

    쉘 업로드 시 스프링 부트가 사용이 안되는 문제 해결 시도

--- scripts/gh_deploy.sh
+++ scripts/gh_deploy.sh
@@ -7,18 +7,12 @@ DEPLOY_ERR_LOG_PATH="/home/ubuntu/$PROJECT_NAME/deploy_err.log"
 APPLICATION_LOG_PATH="/home/ubuntu/$PROJECT_NAME/application.log"
 echo "===== 이전 프로세스 죽이기 시작 : $(date +%c) =====" >> $DEPLOY_LOG_PATH
 BUILD_JAR=$(ls $JAR_PATH)
-echo "> BUILD_JAR 목록1 $BUILD_JAR and" >> $DEPLOY_LOG_PATH
 JAR_NAME=$(basename $BUILD_JAR)
-echo "> BUILD_JAR 목록2 $BUILD_JAR and" >> $DEPLOY_LOG_PATH
-echo echo "> JAR_NAME 목록1 $JAR_NAME and" >> $DEPLOY_LOG_PATH
-
 echo "> build 파일명: $JAR_NAME" >> $DEPLOY_LOG_PATH
 echo "> build 파일 복사" >> $DEPLOY_LOG_PATH
 cp $BUILD_JAR $DEPLOY_PATH
-echo "> BUILD_JAR 목록3 $BUILD_JAR and" >> $DEPLOY_LOG_PATH
 echo "> 현재 동작중인 어플리케이션 pid 체크" >> $DEPLOY_LOG_PATH
 CURRENT_PID=$(pgrep -f $JAR_NAME)
-echo "> BUILD_JAR 목록4 $BUILD_JAR" >> $DEPLOY_LOG_PATH
 if [ -z $CURRENT_PID ]
 then
   echo "> 현재 동작중인 어플리케이션 존재 X" >> $DEPLOY_LOG_PATH
