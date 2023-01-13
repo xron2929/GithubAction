db5db17b31619432a722acefd6e5bbebeba5e278
consistent <xron2929@gmail.com>
2023-01-14 07:22:47 +0900

    shell update 에러 수정

--- scripts/gh_deploy.sh
+++ scripts/gh_deploy.sh
@@ -6,16 +6,16 @@ DEPLOY_LOG_PATH="/home/ubuntu/$PROJECT_NAME/deploy.log"
 DEPLOY_ERR_LOG_PATH="/home/ubuntu/$PROJECT_NAME/deploy_err.log"
 APPLICATION_LOG_PATH="/home/ubuntu/$PROJECT_NAME/application.log"
 echo "===== 이전 프로세스 죽이기 시작 : $(date +%c) =====" >> $DEPLOY_LOG_PATH
-DEPLOY_PATH_FILE=$(cat $DEPLOY_PATH)
-DEPLOY_PATH_FILE=$(cat $DEPLOY_LOG_PATH)
+DEPLOY_LOG_FILE=$(cat DEPLOY_LOG_PATH)
+DEPLOY_ERR_LOG_FILE=$(cat $DEPLOY_LOG_PATH)
 BUILD_JAR=$(ls $JAR_PATH)
 JAR_NAME=$(basename $BUILD_JAR)
-if [ -n $DEPLOY_PATH_FILE ]
+if [ -n DEPLOY_LOG_FILE ]
 then
-  rm DEPLOY_LOG_PATH
-if [ -n $DEPLOY_PATH_FILE ]
+  rm DEPLOY_LOG_FILE
+if [ -n DEPLOY_ERR_LOG_FILE ]
 then
-  rm DEPLOY_LOG_PATH
+  rm DEPLOY_ERR_LOG_FILE
 echo "> build 파일명: $JAR_NAME" >> $DEPLOY_LOG_PATH
 echo "> build 파일 복사" >> $DEPLOY_LOG_PATH
 cp $BUILD_JAR $DEPLOY_PATH
