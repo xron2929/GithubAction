beadb9264ade2ae6889eac7fa2fd6ba9c524a533
consistent <xron2929@gmail.com>
2023-01-14 04:08:18 +0900

    yml 단위 테스트 & 통합 테스트를 둘다 지원하는 CI / CD를 위해 제작 중 서브repo는 우테코에 계신 분들 블로그 참조해서 좋은 idea 같아서 찾아봤는데 properties를 한꺼번에 같이 관리하면 이걸 생성할 때 마다 가져오고, 제외 시켜야된다는 거면 오히려 귀찮은 작업이라, 이걸 CI/CD 조건과 본인이 실행하는 로컬 환경에도 적합하게 맞추는 방법이 없을까 싶어서 git/github/shell 쪽 자료 뒤져봤는데 딱히 괜찮은게 없어서 스프링부트에서 세팅하는게 맞다고 판단

---
+++ src/test/java/dev/be/githubaction/GithubActionLocalTests.java
@@ -0,0 +1,32 @@
+package dev.be.githubaction;
+
+import dev.be.Member;
+import dev.be.repository.MemberRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ActiveProfiles("localtest")
+@SpringBootTest
+public class GithubActionLocalTests {
+    @Autowired
+    MemberRepository memberRepository;
+    @Test
+    void contextLoads() {
+        System.out.println("memberRepository = " + memberRepository);
+    }
+
+    @Test
+    void saveTest() {
+        Member member = new Member();
+        member.setUuid("hello");
+        memberRepository.save(member);
+        assertThat(member).isNotNull();
+        System.out.println("member = " + member);
+        Member member1 = memberRepository.findById(member.getId()).get();
+        System.out.println("member1 = " + member1);
+    }
+}
