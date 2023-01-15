d08b796363b9a9a5ba1de57b1155942af11a29f2
consistent <xron2929@gmail.com>
2023-01-16 07:06:03 +0900

    export 실행 에러 고침

--- scripts/gh_deploy.sh
+++ scripts/gh_deploy.sh
@@ -3,8 +3,8 @@ PROJECT_NAME="github_action"
 JAR_PATH="/home/ubuntu/github_action/build/libs/*.jar"
 DEPLOY_PATH=/home/ubuntu/$PROJECT_NAME/
 DEPLOY_LOG_PATH="/home/ubuntu/$PROJECT_NAME/deploy.log"
-DEPLOY_ERR_LOG_PATH="/home/ubuntu/$PROJECT_NAME/deploy_err.log"
-APPLICATION_LOG_PATH="/home/ubuntu/$PROJECT_NAME/application.log"
+export DEPLOY_ERR_LOG_PATH="/home/ubuntu/$PROJECT_NAME/deploy_err.log"
+export APPLICATION_LOG_PATH="/home/ubuntu/$PROJECT_NAME/application.log"
 echo "===== 이전 프로세스 죽이기 시작 : $(date +%c) =====" >> $DEPLOY_LOG_PATH
 BUILD_JAR=$(ls $JAR_PATH)
 JAR_NAME=$(basename $BUILD_JAR)
@@ -35,11 +35,8 @@ else
   kill -9 $CURRENT_PID
 fi
 echo "===== 배포 시작 : $(date +%c) =====" >> $DEPLOY_LOG_PATH
-DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
+export DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
 echo "> DEPLOY_JAR 배포" >> $DEPLOY_LOG_PATH
-export $DEPLOY_JAR;
-export $APPLICATION_LOG_PATH;
-export $EPLOY_ERR_LOG_PATH
 sh /home/ubuntu/security-sh/githubAtion.sh
 sleep 3
 
